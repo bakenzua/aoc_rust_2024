2d4dc5c22cc805b42748ee2ac914c7ca6806aef9
Barney Akenzua-Sanderson <barnaby.sanderson@gstt.nhs.uk>
2024-12-15 15:13:12 +0000

    cargo fmt

--- src/day_14.rs
+++ src/day_14.rs
@@ -4,7 +4,6 @@ use std::{collections::HashMap, fs::read_to_string};
 
 use itertools::Itertools;
 use regex::Regex;
-// use itertools::
 
 const EXAMPLE_FILEPATH: &str = "./data/example_14.txt";
 const EXAMPLE_MAP_SIZE: (i64, i64) = (11, 7);
@@ -90,36 +89,35 @@ fn part_2(file_path: &str, map_size: &(i64, i64)) -> i64 {
 
         let bin_size = 5_f64;
 
-        let entropy: f64 = robot_positions_after_step.iter()
-        .map(
-            |robot| 
+        let entropy: f64 = robot_positions_after_step
+            .iter()
+            .map(|robot| 
             // get_quadrant(robot, map_size)
             (
                 (robot.0 as f64 / bin_size).floor() as i64,
                 (robot.1 as f64 / bin_size).floor() as i64
-            )
-        )
-        .counts()
-        .values()
-        .map(
-            |count| 
-            if *count > 0 {
-                let p: f64 = (*count as f64 )/ (robots.len() as f64);
-                -1_f64 * (p * p.log2())
-            } else {
-                0_f64
-            }
-        )
-        .sum();
+            ))
+            .counts()
+            .values()
+            .map(|count| {
+                if *count > 0 {
+                    let p: f64 = (*count as f64) / (robots.len() as f64);
+                    -1_f64 * (p * p.log2())
+                } else {
+                    0_f64
+                }
+            })
+            .sum();
 
         entropies.insert(step, entropy);
     }
 
     // Find the key with the smallest value, is with smallest entropy
-    entropies.iter()
+    entropies
+        .iter()
         .min_by(|a, b| a.1.partial_cmp(b.1).unwrap())
-        .map(|(key, _value)| *key).unwrap()
-
+        .map(|(key, _value)| *key)
+        .unwrap()
 }
 
 fn parse_input(file_txt: String) -> Vec<((i64, i64), (i64, i64))> {
@@ -196,8 +194,14 @@ mod tests {
 
     #[test]
     fn test_get_quadrant() {
-        assert_eq!(get_quadrant(&(0_i64, 0_i64), &INPUT_MAP_SIZE), Some(Quadrant::One));
-        assert_eq!(get_quadrant(&(52_i64, 0_i64), &INPUT_MAP_SIZE), Some(Quadrant::Two));
+        assert_eq!(
+            get_quadrant(&(0_i64, 0_i64), &INPUT_MAP_SIZE),
+            Some(Quadrant::One)
+        );
+        assert_eq!(
+            get_quadrant(&(52_i64, 0_i64), &INPUT_MAP_SIZE),
+            Some(Quadrant::Two)
+        );
         assert_eq!(get_quadrant(&(50_i64, 0_i64), &INPUT_MAP_SIZE), None);
         assert_eq!(get_quadrant(&(50_i64, 52_i64), &INPUT_MAP_SIZE), None)
     }
